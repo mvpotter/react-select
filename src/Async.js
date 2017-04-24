09160646f8d5ff40fb7994077c1e9cb005c835bc
Mikhail Potter <mikhail.potter@noveogroup.com>
2017-04-24 16:01:25 +0700

    Fix issue with onInputChange return value in AsyncCreatable component

--- src/Async.js
+++ src/Async.js
@@ -149,7 +149,7 @@ export default class Async extends Component {
 		}
 
 		if (onInputChange) {
-			onInputChange(inputValue);
+			inputValue = onInputChange(inputValue);
 		}
 
 		return this.loadOptions(inputValue);
